fd20578be8f15707e9a6c3cd7c5559d825a34847
Jackie DiMonte <jdimonte@chicagobooth.edu>
2017-08-18 00:18:43 +0000

    cycles-4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,19 +11,27 @@ class UsersController < ApplicationController
     @addresses = Address.all
     @orders = Order.all
     @cycles = Cycle.all
+    @shipments = Shipment.all
     
     @address = Address.new
     @order = Order.new
     @cycle = Cycle.new
-    
+    @shipment = Shipment.new
 
     @user_cycles = Cycle.where(:user_id => current_user.id)
     
-    @currentcycle = @user_cycles.order("start_date DESC").first
+    @recentcycle = @user_cycles.order("start_date DESC").first
     
-    @nextcyclestart = @currentcycle.start_date + 30
-    @nextcycledend = @currentcycle.end_date + 30
+    @nextcyclestart = @recentcycle.start_date + 30
+    @nextcycleend = @recentcycle.end_date + 30
     
+    @recentaddress = @addresses.order("updated_at DESC").first
+    @recentorder = @orders.order("updated_at DESC").first
+
+  # YOU WILL NEED TO CHANGE THIS  
+  # @recentshipment = @shipments.order("updated_at DESC").first
+    @nextshipment = @recentcycle.start_date + 25
+
     
     render("/users/show.html.erb")
   end
